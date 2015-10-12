855f08b121abb4a3591fad64a11bfc9e480bc70f
Mohammed Basith <basith374@gmail.com>
2015-10-12 22:49:25 +0530

    more_icons

--- app/scripts/services/sidenav.js
+++ app/scripts/services/sidenav.js
@@ -13,41 +13,41 @@ angular.module('bluroeApp')
     // ...
 
     var AsideListHome = [
-        {'id':'1','type':'link','name':'Feeds','ngClick':'home','img':'icons/fb-50.png'},
-        {'id':'2','type':'link','name':'My Tasks','ngClick':'mytasks','img':'icons/twitter-50.png'},
-        {'id':'3','type':'link','name':'My Milestones','ngClick':'mymilestones','img':'icons/gplus-50.png'},
-        {'id':'4','type':'link','name':'My Bugs','ngClick':'mybugs','img':'icons/twitter-50.png'},
-        {'id':'5','type':'link','name':'My Calender','ngClick':'mycalendar','img':'icons/fb-50.png'},
-        {'id':'6','type':'link','name':'Reports ','ngClick':'reports','img':'icons/gplus-50.png'},
+        {'id':'1','type':'link','name':'Feeds','ngClick':'home','img':'icons/fb-50.png', 'icon':'fa fa-feed'},
+        {'id':'2','type':'link','name':'My Tasks','ngClick':'mytasks','img':'icons/twitter-50.png', 'icon':'fa fa-tasks'},
+        {'id':'3','type':'link','name':'My Milestones','ngClick':'mymilestones','img':'icons/gplus-50.png', 'icon':'fa fa-map-marker'},
+        {'id':'4','type':'link','name':'My Bugs','ngClick':'mybugs','img':'icons/twitter-50.png', 'icon':'fa fa-bug'},
+        {'id':'5','type':'link','name':'My Calender','ngClick':'mycalendar','img':'icons/fb-50.png', 'icon':'fa fa-calendar'},
+        {'id':'6','type':'link','name':'Reports ','ngClick':'reports','img':'icons/gplus-50.png', 'icon':'fa fa-exclamation-circle'},
         //{'type':'linkList','name':'Extras','img':'icons/twitter-50.png','list': [
         //    {'id':'7','type':'link','name':'Themes','ngClick':'themes','img':'icons/fb-50.png'},
         //    {'id':'8','type':'link','name':'Templates','ngClick':' ','img':'icons/gplus-50.png'},
         //]},
 
-        {'id':'7','type':'link','name':'Themes','ngClick':'themes','img':'icons/fb-50.png'},
+        {'id':'7','type':'link','name':'Themes','ngClick':'themes','img':'icons/fb-50.png', 'icon':'fa fa-sliders'},
     ];
 
         var AsideListProjectIndex = [
-            {'id':'1','type':'link','name':'Active projects','ngClick':'projects','img':'icons/gplus-50.png'},
-            {'id':'2','type':'link','name':'Public projects','ngClick':' ','img':'icons/twitter-50.png'},
+            {'id':'1','type':'link','name':'Active projects','ngClick':'projects','img':'icons/gplus-50.png', 'icon':'fa fa-play'},
+            {'id':'2','type':'link','name':'Public projects','ngClick':' ','img':'icons/twitter-50.png', 'icon':'fa fa-building'},
         ];
 
     var AsideListProject = [
-        {'id':'1','type':'link','name':'Feeds','ngClick':' ','img':'icons/gplus-50.png'},
-        {'id':'2','type':'link','name':'Task List','ngClick':' ','img':'icons/twitter-50.png'},
-        {'id':'8','type':'link','name':'Task','ngClick':' ','img':'icons/twitter-50.png'},
-        {'type':'linkList','name':'Project Tools','img':'icons/fb-50.png','list': [
+        {'id':'1','type':'link','name':'Feeds','ngClick':' ','img':'icons/gplus-50.png', 'icon':'fa fa-feed'},
+        {'id':'2','type':'link','name':'Task List','ngClick':' ','img':'icons/twitter-50.png', 'icon':'fa fa-tasks'},
+        {'id':'8','type':'link','name':'Task','ngClick':' ','img':'icons/twitter-50.png', 'icon':'fa fa-check'},
+        {'type':'linkList','name':'Project Tools','img':'icons/fb-50.png','icon':'fa fa-wrench','list': [
             {'id':'3','type':'link','name':'Tools 1','ngClick':' ','img':'icons/fb-50.png'},
             {'id':'4','type':'link','name':'Tools 2','ngClick':' ','img':'icons/gplus-50.png'},
             {'id':'5','type':'link','name':'Tools 3','ngClick':' ','img':'icons/twitter-50.png'},
         ]},
-        {'type':'linkList','name':'Other Details','img':'icons/fb-50.png','list': [
+        {'type':'linkList','name':'Other Details','img':'icons/fb-50.png', 'icon':'fa fa-list', 'list': [
             {'id':'9','type':'link','name':'SRS','ngClick':' ','img':'icons/fb-50.png'},
             {'id':'10','type':'link','name':'Project Requirements','ngClick':' ','img':'icons/gplus-50.png'},
             {'id':'11','type':'link','name':'Project Policies','ngClick':' ','img':'icons/twitter-50.png'},
             {'id':'12','type':'link','name':'Project Rules','ngClick':' ','img':'icons/twitter-50.png'},
         ]},
-        {'id':'7','type':'link','name':'Settings','ngClick':' ','img':'icons/gplus-50.png'},
+        {'id':'7','type':'link','name':'Settings','ngClick':' ','img':'icons/gplus-50.png', 'icon':'fa fa-cog'},
     ];
 
 

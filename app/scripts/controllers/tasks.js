dfe5232a3bf592fa6e392883eb2c9976a3142ca4
unknown <shibinarakkan@gmail.com>
2015-10-15 23:42:52 +0530

    goofi2

--- app/scripts/controllers/tasks.js
+++ app/scripts/controllers/tasks.js
@@ -25,10 +25,28 @@ angular.module('bluroeApp')
     }
 
     this.tasks = [
-    	{'name':'Shizu','type':'taskList'},
-    	{'name':'Shizu','type':'taskList'},
-    	{'name':'Shizu','type':'taskList'},
-    	{'name':'Shizu','type':'taskList'},
-    	{'name':'Shizu','type':'taskList'}
+    	{'name':'Shizu','type':'taskList','children':[
+            {'name':'Shit happening','owner':'shaji','start':'-','due':'-','duration':'3','progress':20,'createdBy':'basi'},
+            {'name':'Get ready','owner':'shaji','start':'-','due':'-','duration':'2','progress':30,'createdBy':'alma'},
+            {'name':'Make the crowd','owner':'shaji','start':'-','due':'-','duration':'5','progress':40,'createdBy':'soshi'},
+        ]}, 
+        {'name':'Rozario','type':'taskList','children':[ 
+            {'name':'Make the crowd','owner':'shaji','start':'-','due':'-','duration':'5','progress':40,'createdBy':'soshi'},
+        ]}, 
+        {'name':'Pragdo','type':'taskList','children':[
+            {'name':'Make the crowd','owner':'shaji','start':'-','due':'-','duration':'5','progress':40,'createdBy':'soshi'},
+            {'name':'Man on fire','owner':'shaji','start':'-','due':'-','duration':'3','progress':20,'createdBy':'basi'},
+            {'name':'Get ready','owner':'shaji','start':'-','due':'-','duration':'2','progress':30,'createdBy':'alma'},
+        ]}, 
+        {'name':'Seamleass','type':'taskList','children':[
+            {'name':'Blue ride','owner':'shaji','start':'-','due':'-','duration':'3','progress':20,'createdBy':'basi'},
+            {'name':'Get ready','owner':'shaji','start':'-','due':'-','duration':'2','progress':30,'createdBy':'alma'},
+            {'name':'Make the crowd','owner':'shaji','start':'-','due':'-','duration':'5','progress':40,'createdBy':'soshi'},
+            {'name':'Man on fire','owner':'shaji','start':'-','due':'-','duration':'3','progress':20,'createdBy':'basi'},
+            {'name':'Get ready','owner':'shaji','start':'-','due':'-','duration':'2','progress':30,'createdBy':'alma'},
+        ]}, 
+        {'name':'Sample ready','type':'task','owner':'shaji','start':'-','due':'-','duration':'2','progress':30,'createdBy':'alma'},
+        {'name':'yeah ready','type':'task','owner':'shaji','start':'-','due':'-','duration':'2','progress':30,'createdBy':'alma'},
+        {'name':'Shitzu ready','type':'task','owner':'shaji','start':'-','due':'-','duration':'2','progress':30,'createdBy':'alma'},
     ]
   });

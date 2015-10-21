d27294f6048f52a0c6a249b86528bc9850571ecb
waxx04 <rinasmusthafa@gmail.com>
2015-10-22 03:25:49 +0530

    shitshaa

--- app/scripts/controllers/milestones.js
+++ app/scripts/controllers/milestones.js
@@ -8,14 +8,37 @@
  * Controller of the bluroeApp
  */
 angular.module('bluroeApp')
-  .controller('MilestonesCtrl', function () {
-    
-  	this.milestones = [
-  		{id:1,name:'Shitzu',owner:'prickzen',start_date:'today',end_date:'tommorow',tasks:'10'},
-  		{id:1,name:'Shitzu',owner:'prickzen',start_date:'today',end_date:'tommorow',tasks:'10'},
-  		{id:1,name:'Shitzu',owner:'prickzen',start_date:'today',end_date:'tommorow',tasks:'10'},
-  		{id:1,name:'Shitzu',owner:'prickzen',start_date:'today',end_date:'tommorow',tasks:'10'},
-  		{id:1,name:'Shitzu',owner:'prickzen',start_date:'today',end_date:'tommorow',tasks:'10'}
-  	];
+  .controller('MilestonesCtrl', function (navLine) { 
+    var vm = this;
+
+    vm.selectedTab = 1; 
+    navLine.navLine($('#myTasksNavLine'),vm.selectedTab);  
+  	vm.selectTab = function(tab) {
+        vm.selectedTab = tab; 
+        var ele =  $('#myTasksNavLine');
+        navLine.navLine(ele,tab); 
+    } 
+
+    vm.isSelected = function(tab) {
+        return vm.selectedTab == tab;
+    }
+
+    Task.onFetch(function() {
+        vm.tasklists = Task.getTaskLists();
+        console.log(vm.tasklists);
+    });
+
+    vm.tlist = [
+        {'title':'task 1', 'released':0,'description':'This task is a task that you cant even tast the task of the task, because you already know that this task is not the real task','priority':1,'startDate':'dec 10','endDate':'dec 30','duration':'7'}, 
+        {'title':'task 1', 'released':1,'description':'This task is a task that you cant even tast the task of the task, because you already know that this task is not the real task','priority':0,'startDate':'dec 13','endDate':'dec 14','duration':'2'}, 
+        {'title':'task 1', 'released':1,'description':'This task is a task that you cant even tast the task of the task, because you already know that this task is not the real task','priority':3,'startDate':'dec 10','endDate':'dec 14','duration':'8'}, 
+        {'title':'task 1', 'released':0,'description':'This task is a task that you cant even tast the task of the task, because you already know that this task is not the real task','priority':2,'startDate':'dec 10','endDate':'dec 14','duration':'5'}, 
+        {'title':'task 1', 'released':0,'description':'This task is a task that you cant even tast the task of the task, because you already know that this task is not the real task','priority':1,'startDate':'dec 7','endDate':'dec 14','duration':'2'}, 
+        {'title':'task 1', 'released':1,'description':'This task is a task that you cant even tast the task of the task, because you already know that this task is not the real task','priority':3,'startDate':'dec 10','endDate':'dec 23','duration':'6'}, 
+        {'title':'task 1', 'released':1,'description':'This task is a task that you cant even tast the task of the task, because you already know that this task is not the real task','priority':0,'startDate':'dec 10','endDate':'dec 24','duration':'7'}, 
+        {'title':'task 1', 'released':0,'description':'This task is a task that you cant even tast the task of the task, because you already know that this task is not the real task','priority':3,'startDate':'dec 02','endDate':'dec 14','duration':'2'}, 
+        {'title':'task 1', 'released':1,'description':'This task is a task that you cant even tast the task of the task, because you already know that this task is not the real task','priority':2,'startDate':'dec 10','endDate':'dec 14','duration':'4'}, 
+        {'title':'task 1', 'released':0,'description':'This task is a task that you cant even tast the task of the task, because you already know that this task is not the real task','priority':0,'startDate':'dec 1','status':'Comleted','duration':'7'}, 
+    ];
 
   });

59ec5b99a8cf0086ca53db7817d0f2ad70441139
waxx04 <rinasmusthafa@gmail.com>
2015-10-22 10:25:32 +0530

    shitScal

--- app/scripts/controllers/tasks.js
+++ app/scripts/controllers/tasks.js
@@ -8,7 +8,7 @@
  * Controller of the bluroeApp
  */
 angular.module('bluroeApp')
-  .controller('TasksCtrl', function (navLine, Task, $scope, Hoster,statusBar) {
+  .controller('TasksCtrl', function (navLine, Task, $scope, Hoster,statusBar,story) {
 
     statusBar.updateStatus('From here you can view user stories and add them to release backlogs');
 
@@ -82,46 +82,19 @@ angular.module('bluroeApp')
         {'id':28,'title':'Trapped module refactoring', 'released':false,'description':'Super awesome task you will love it i know ...!!','priority':3,'startDate':'dec 02','endDate':'dec 14','duration':'2','durationUnit':'h'}, 
         
     ];
-
-    vm.tlist = [ 
-        {'id':1,'title':'Refactoring Iceblocks', 'released':false,'description':'Cool tasks are always cool. So do this !!','priority':1,'startDate':'dec 10','endDate':'dec 30','duration':'7','durationUnit':'h'}, 
-        {'id':2,'title':'Streaming section', 'released':false,'description':'This is one of the besttask ever i ve seen in my life time : )','priority':0,'startDate':'dec 13','endDate':'dec 14','duration':'2','durationUnit':'h'}, 
-        {'id':3,'title':'Urgent Completion', 'released':true,'description':'This is a urgent task . U should make the system flow for ever','priority':3,'startDate':'dec 10','endDate':'dec 14','duration':'8','durationUnit':'h'}, 
-        {'id':4,'title':'Elangent Model shift', 'released':false,'description':'Super awesome task you will love it i know ...!!','priority':2,'startDate':'dec 10','endDate':'dec 14','duration':'5','durationUnit':'h'}, 
-        {'id':5,'title':'Bug trakking system', 'released':false,'description':'This task is for powerful people.. Yeah you..! ','priority':1,'startDate':'dec 7','endDate':'dec 14','duration':'2','durationUnit':'h'}, 
-        {'id':6,'title':'Featured blog completion', 'released':false,'description':'This is a urgent task . U should make the system flow for ever','priority':3,'startDate':'dec 10','endDate':'dec 23','duration':'6','durationUnit':'h'}, 
-        {'id':7,'title':'Quick finish', 'released':true,'description':'Super awesome task you will love it i know it, can you just finish this.. ? sothat we can move On..','priority':0,'startDate':'dec 10','endDate':'dec 24','duration':'7','durationUnit':'h'}, 
-        {'id':8,'title':'Trapped module refactoring', 'released':false,'description':'Super awesome task you will love it i know ...!!','priority':3,'startDate':'dec 02','endDate':'dec 14','duration':'2','durationUnit':'h'}, 
-        {'id':9,'title':'Smooth UI Design', 'released':true,'description':'This is a urgent task . U should make the system flow for ever','priority':2,'startDate':'dec 10','endDate':'dec 14','duration':'4','durationUnit':'h'}, 
-        {'id':10,'title':'Refactoring Iceblocks', 'released':false,'description':'Major refactor needed in the sublimentory section of zylanfuzku Masked version.Its all about the stuffs and stone of the rechard steven.','priority':0,'startDate':'dec 1','status':'Comleted','duration':'7','durationUnit':'h'}, 
-        {'id':11,'title':'Urgent Completion', 'released':true,'description':'This is a urgent task . U should make the system flow for ever','priority':3,'startDate':'dec 10','endDate':'dec 14','duration':'8','durationUnit':'h'}, 
-        {'id':12,'title':'Elangent Model shift', 'released':false,'description':'Super awesome task you will love it i know ...!!','priority':2,'startDate':'dec 10','endDate':'dec 14','duration':'5','durationUnit':'h'}, 
-        {'id':13,'title':'Bug trakking system', 'released':false,'description':'This task is for powerful people.. Yeah you..! ','priority':1,'startDate':'dec 7','endDate':'dec 14','duration':'2','durationUnit':'h'}, 
-        {'id':14,'title':'Featured blog completion', 'released':false,'description':'This is a urgent task . U should make the system flow for ever','priority':3,'startDate':'dec 10','endDate':'dec 23','duration':'6','durationUnit':'h'}, 
-        {'id':15,'title':'Quick finish', 'released':true,'description':'Super awesome task you will love it i know it, can you just finish this.. ? sothat we can move On..','priority':0,'startDate':'dec 10','endDate':'dec 24','duration':'7','durationUnit':'h'}, 
-        {'id':16,'title':'Trapped module refactoring', 'released':false,'description':'Super awesome task you will love it i know ...!!','priority':3,'startDate':'dec 02','endDate':'dec 14','duration':'2','durationUnit':'h'}, 
-        {'id':17,'title':'Quick finish', 'released':true,'description':'Super awesome task you will love it i know it, can you just finish this.. ? sothat we can move On..','priority':0,'startDate':'dec 10','endDate':'dec 24','duration':'7','durationUnit':'h'}, 
-        {'id':18,'title':'Trapped module refactoring', 'released':false,'description':'Super awesome task you will love it i know ...!!','priority':3,'startDate':'dec 02','endDate':'dec 14','duration':'2','durationUnit':'h'}, 
-        {'id':19,'title':'Smooth UI Design', 'released':false,'description':'This is a urgent task . U should make the system flow for ever','priority':2,'startDate':'dec 10','endDate':'dec 14','duration':'4','durationUnit':'h'}, 
-        {'id':20,'title':'Refactoring Iceblocks', 'released':false,'description':'Major refactor needed in the sublimentory section of zylanfuzku Masked version.Its all about the stuffs and stone of the rechard steven.','priority':0,'startDate':'dec 1','status':'Comleted','duration':'7','durationUnit':'h'}, 
-        {'id':21,'title':'Urgent Completion', 'released':true,'description':'This is a urgent task . U should make the system flow for ever','priority':3,'startDate':'dec 10','endDate':'dec 14','duration':'8','durationUnit':'h'}, 
-        {'id':22,'title':'Elangent Model shift', 'released':false,'description':'Super awesome task you will love it i know ...!!','priority':2,'startDate':'dec 10','endDate':'dec 14','duration':'5','durationUnit':'h'}, 
-        {'id':23,'title':'Bug trakking system', 'released':false,'description':'This task is for powerful people.. Yeah you..! ','priority':1,'startDate':'dec 7','endDate':'dec 14','duration':'2','durationUnit':'h'}, 
-        {'id':24,'title':'Featured blog completion', 'released':false,'description':'This is a urgent task . U should make the system flow for ever','priority':3,'startDate':'dec 10','endDate':'dec 23','duration':'6','durationUnit':'h'}, 
-        {'id':25,'title':'Quick finish', 'released':true,'description':'Super awesome task you will love it i know it, can you just finish this.. ? sothat we can move On..','priority':0,'startDate':'dec 10','endDate':'dec 24','duration':'7','durationUnit':'h'}, 
-        {'id':26,'title':'Trapped module refactoring', 'released':false,'description':'Super awesome task you will love it i know ...!!','priority':3,'startDate':'dec 02','endDate':'dec 14','duration':'2','durationUnit':'h'}, 
-        {'id':27,'title':'Quick finish', 'released':true,'description':'Super awesome task you will love it i know it, can you just finish this.. ? sothat we can move On..','priority':0,'startDate':'dec 10','endDate':'dec 24','duration':'7','durationUnit':'h'}, 
-        {'id':28,'title':'Trapped module refactoring', 'released':false,'description':'Super awesome task you will love it i know ...!!','priority':3,'startDate':'dec 02','endDate':'dec 14','duration':'2','durationUnit':'h'}, 
-        
-    ];
+ 
     vm.keyPressed = false;
     vm.titleSave = function ($event,data){
         if($event.keyCode == 13){ 
             data.titleEdit = false; 
             data.title = $event.target.value;
             $event.target.blur();
-            vm.keyPressed = true;
-            statusBar.updateStatus('Title saved');
+            vm.keyPressed = true; 
+            story.updateStory({story:data.id,name:data.title},function(){
+                statusBar.updateStatus('Title saved')
+            },function(){
+                statusBar.updateStatus('SERVER ERROR : Title is not saved')
+            });
         }else
         if($event.keyCode == 27){ 
             data.titleEdit = false;  
@@ -139,7 +112,11 @@ angular.module('bluroeApp')
             data.description = $event.target.value;
             $event.target.blur();
             vm.keyPressed = true;
-            statusBar.updateStatus('Description saved');
+            story.updateStory({story:data.id,name:data.description},function(){
+                statusBar.updateStatus('Description saved')
+            },function(){
+                statusBar.updateStatus('SERVER ERROR : Description is not saved')
+            });
         }else
         if($event.keyCode == 27){  
             data.descEdit = false; 
@@ -178,34 +155,55 @@ angular.module('bluroeApp')
         statusBar.updateStatus('Now you can edit the Description');
     }
     vm.markModeVar = false;
+    vm.processingMB = false;
     vm.markedCart = [];
     var markedStoriesName = '';
     vm.markMode = function(){
         if(vm.markModeVar){  
             if(vm.selectedTab == 1){
                 if(vm.markedCart.length < 1){
-                    statusBar.updateStatus( '0 stories are added to the release log');
-                }else{
-                    angular.forEach(vm.markedCart, function (data, index) { 
-                        data.marked = false;
-                        data.released = true;
+                    statusBar.updateStatus( 'Nothing was added to the release log');
+                }else{ 
+                    vm.processingMB = true;
+                    var storyList = vm.markedCart.map(function(value,index) { return value['id']; });
+                    story.addToSprint({sprint:1,stories:storyList},function(){
+                        statusBar.updateStatus(vm.markedCart.length + ' stories were added to the release log');
+                        angular.forEach(vm.markedCart, function (data, index) { 
+                            data.marked = false;
+                            data.released = true; 
+                        });  
+                        vm.markedCart = [];
+                        vm.processingMB = false;
+                    },function(){
+                        statusBar.updateStatus('SERVER ERROR : Stories were not added to the release log'); 
+                        vm.processingMB = false;
                     });
-                    statusBar.updateStatus(vm.markedCart.length + 'strories are added to the release log'); 
                 }
             }else if(vm.selectedTab == 2){
                 if(vm.markedCart.length < 1){
-                    statusBar.updateStatus('0 stories are removed from the release log');
+                    statusBar.updateStatus('Nothing was removed from the release log');
                 }else{
                     angular.forEach(vm.markedCart, function (data, index) { 
                         data.marked = false;
-                        data.released = false;
+                        data.released = false;  
+                        vm.markedCart = [];
+                    }); 
+                    story.removeFromSprint({sprint:1,stories:storyList},function(){
+                        statusBar.updateStatus(vm.markedCart.length + ' stories were removed to the release log');
+                        angular.forEach(vm.markedCart, function (data, index) { 
+                            data.marked = false;
+                            data.released = true; 
+                        });  
+                        vm.markedCart = [];
+                        vm.processingMB = false;
+                    },function(){
+                        statusBar.updateStatus('SERVER ERROR : Stories were not removed to the release log'); 
+                        vm.processingMB = false;
                     });
-                    statusBar.updateStatus(vm.markedCart.length + ' stories are removed from the release log');
                 }
             } 
             markedStoriesName = '';
             vm.markModeVar = false;
-            vm.markedCart = [];
         }else{
             vm.markModeVar = true;
             statusBar.updateStatus('Mark mode is on');
